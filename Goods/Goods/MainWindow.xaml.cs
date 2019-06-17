7d7ea12556897964f2df448317870f838e4bfe17
fishMD <fishmd007@gmail.com>
2019-06-18 00:26:34 +0300

    + add goods  controls, + googds by switlana

--- Goods/Goods/MainWindow.xaml.cs
+++ Goods/Goods/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.VisualBasic;
 
 namespace Goods
 {
@@ -23,6 +24,13 @@ namespace Goods
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void New_Category_Click(object sender, RoutedEventArgs e)
+        {
+            String s = Microsoft.VisualBasic.Interaction.InputBox("Введіть нову категорію");
+            if (s.Trim() == "")
+                return;
         }
     }
 }
